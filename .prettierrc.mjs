85d0b252bec8aeabccdf25bca31992d5b8b067bc
Dawson Booth <dawson@dawsonbooth.com>
2025-04-03 17:53:12 -0500

    Set up editor stuff and linting

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+export default {
+  arrowParens: 'avoid',
+  jsxSingleQuote: false,
+  printWidth: 100,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-organize-imports'],
+}
