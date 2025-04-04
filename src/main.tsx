2d4ee3213271285544e4c20b91b5002b2af0df3c
Dawson Booth <dawson@dawsonbooth.com>
2025-04-03 19:15:42 -0500

    Move components to components folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ErrorBoundary from './error-boundary'
-import EvalBar from './eval-bar/eval-bar.tsx'
+import ErrorBoundary from './components/error-boundary'
+import EvalBar from './components/eval-bar'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
